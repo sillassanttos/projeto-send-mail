925f20c911fd58106cf1c5f4eacb2d1fbd99ac6e
Silas Santos <sillassanttos@outlook.com>
2023-03-04 18:04:41 -0400

    Adicionado envio de email

--- processa_envio.php
+++ processa_envio.php
@@ -48,20 +48,20 @@
       //Server settings
       $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
       $mail->isSMTP();                                            //Send using SMTP
-      $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+      $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
       $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-      $mail->Username   = 'user@example.com';                     //SMTP username
-      $mail->Password   = 'secret';                               //SMTP password
+      $mail->Username   = '';                     //SMTP username
+      $mail->Password   = '';                               //SMTP password
       $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
       $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
       //Recipients
-      $mail->setFrom('from@example.com', 'Mailer');
-      $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-      $mail->addAddress('ellen@example.com');               //Name is optional
-      $mail->addReplyTo('info@example.com', 'Information');
-      $mail->addCC('cc@example.com');
-      $mail->addBCC('bcc@example.com');
+      $mail->setFrom('softsolweb.ssw@gmail.com', 'Mailer');
+      $mail->addAddress('softsolweb.ssw@gmail.com', 'Joe User');     //Add a recipient
+      //$mail->addAddress('ellen@example.com');               //Name is optional
+      //$mail->addReplyTo('info@example.com', 'Information');
+     // $mail->addCC('cc@example.com');
+     // $mail->addBCC('bcc@example.com');
 
       //Attachments
       //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
@@ -69,9 +69,9 @@
 
       //Content
       $mail->isHTML(true);                                  //Set email format to HTML
-      $mail->Subject = 'Here is the subject';
-      $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-      $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+      $mail->Subject = 'Assunto do email';
+      $mail->Body    = 'Conteúdo do e-mail';
+      //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
       $mail->send();
       echo 'Message has been sent';
